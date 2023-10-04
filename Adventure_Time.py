75b0166474be52fb70ec9e65ec5f04534bca8f02
maxi-correa <maxis.c.22@gmail.com>
2023-10-04 17:26:06 -0300

    Cambio variables resolución en Adv_Time.py

--- Adventure_Time.py
+++ Adventure_Time.py
@@ -5,16 +5,17 @@ import pygame #Paquetes necesarios para la creación del juego
 BLACK = (0, 0, 0)
 
 #Definición de resolución de pantalla
-RESOLUCION = (1100, 550)
+ANCHO = 1100
+ALTO = 550
 
 #Clase del juego llamado Adventure Time
 class Adventure_Time:
     def __init__(self):
-        pygame.init()                                      #Inicialización de pygame
-        self.screen = pygame.display.set_mode(RESOLUCION)  #Creación de ventana 1100 px ancho 550 px alto
-        pygame.display.set_caption("Adventure Time")       #Nombre que aparece en ventana
+        pygame.init()                                         #Inicialización de pygame
+        self.screen = pygame.display.set_mode((ANCHO, ALTO))  #Creación de ventana 1100 px ancho 550 px alto
+        pygame.display.set_caption("Adventure Time")          #Nombre que aparece en ventana
     
-    def corre_juego(self):                                 #Método necesario para que el juego corra
+    def corre_juego(self):                                    #Método necesario para que el juego corra
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
