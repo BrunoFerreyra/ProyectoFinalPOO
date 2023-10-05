02588c01f7c6fd176968fc4098e0abc850005633
maxi-correa <maxis.c.22@gmail.com>
2023-10-04 23:54:12 -0300

    clase Boton

--- Adventure_Time.py
+++ Adventure_Time.py
@@ -1,29 +1,46 @@
+#Paquetes necesarios para la creación del juego
 import sys
-import pygame #Paquetes necesarios para la creación del juego
+import pygame
+import boton
 
 #Definición de colores
-BLACK = (0, 0, 0)
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
 
 #Definición de resolución de pantalla
-ANCHO = 1100
-ALTO = 550
+PANTALLA_ANCHO = 1100
+PANTALLA_ALTO = 550
 
-#Clase del juego llamado Adventure Time
-class Adventure_Time:
-    def __init__(self):
-        pygame.init()                                         #Inicialización de pygame
-        self.screen = pygame.display.set_mode((ANCHO, ALTO))  #Creación de ventana 1100 px ancho 550 px alto
-        pygame.display.set_caption("Adventure Time")          #Nombre que aparece en ventana
-    
-    def corre_juego(self):                                    #Método necesario para que el juego corra
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+#Creación de ventana 1100 px ancho 550 px alto
+screen = pygame.display.set_mode((PANTALLA_ANCHO, PANTALLA_ALTO))  
+#Nombre que aparece en ventana
+pygame.display.set_caption("Adventure Time")
+
+#Carga de imagenes de botones
+play_img = pygame.image.load('botones/play_btn.png').convert_alpha()
+exit_img = pygame.image.load('botones/exit_btn.png').convert_alpha()
+play_img.set_colorkey(WHITE)
+exit_img.set_colorkey(WHITE)
+
+#Instancias de botones
+boton_play = boton.Boton(400, 200, play_img, 1.5)
+boton_exit = boton.Boton(400, 300, exit_img, 1.5)
+
+run = True
+while run:
+
+    screen.fill(WHITE)
+
+    if boton_play.dibujar(screen):
+        print('PLAY')
+    if boton_exit.dibujar(screen):
+        run = False
             
-            self.screen.fill(BLACK)
-            pygame.display.update()                        #Actualiza la pantalla en cada ciclo
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
-if __name__ == "__main__":                                 #Se crea el objeto juego y luego se llama al método que lo hace correr
-    adventure_time_juego = Adventure_Time()
-    adventure_time_juego.corre_juego()
+    #Actualiza la pantalla en cada ciclo
+    pygame.display.update()
+
+pygame.quit()
