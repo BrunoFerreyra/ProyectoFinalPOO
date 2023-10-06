f6d95aa8ece1f21e8aa9a18a6f54041df6eff743
maxi-correa <maxis.c.22@gmail.com>
2023-10-06 00:48:39 -0300

    Sonido de menú. README.

--- Adventure_Time.py
+++ Adventure_Time.py
@@ -3,6 +3,8 @@ import sys
 import pygame
 from boton import Boton
 
+pygame.init()
+
 #Definición de colores
 BLACK = (  0,   0,   0)
 WHITE = (255, 255, 255)
@@ -11,11 +13,10 @@ CYAN =  (  0, 255, 255)
 #Definición de resolución de pantalla
 PANTALLA_ANCHO = 800
 PANTALLA_ALTO = 600
-x = 0
+movimiento_fondo = 0
 
-#Creación de ventana
+#Creación y nombre de ventana
 screen = pygame.display.set_mode((PANTALLA_ANCHO, PANTALLA_ALTO))  
-#Nombre que aparece en ventana
 pygame.display.set_caption("Adventure Time")
 
 #Carga de imagenes
@@ -25,6 +26,10 @@ play_img = pygame.image.load('imagenes/play_btn.png').convert_alpha()
 option_img = pygame.image.load('imagenes/option_btn.png').convert_alpha()
 quit_img = pygame.image.load('imagenes/quit_btn.png').convert_alpha()
 
+#Carga de música de fondo
+pygame.mixer.music.load('sonidos/menu_theme.ogg')
+pygame.mixer.music.play(-1) #-1 para reproducción infinita
+
 #Instancias de botones
 boton_play = Boton(300, 250, play_img, 1)
 boton_option = Boton(300, 350, option_img, 1)
@@ -33,12 +38,15 @@ boton_quit = Boton(300, 450, quit_img, 1)
 run = True
 while run:
     
-    x_relativa = x % fondo_img.get_rect().width
-    screen.blit(fondo_img, (x_relativa-fondo_img.get_rect().width,0))
-    if x_relativa < PANTALLA_ANCHO:
-        screen.blit(fondo_img, (x_relativa,0))
-    x -= 1
+    #Fondo en movimiento
+    movimiento_fondo_relativo = movimiento_fondo % fondo_img.get_rect().width
+    screen.blit(fondo_img, (movimiento_fondo_relativo - fondo_img.get_rect().width,0))
+    if movimiento_fondo_relativo < PANTALLA_ANCHO:
+        screen.blit(fondo_img, (movimiento_fondo_relativo,0))
+    movimiento_fondo -= 1
     
+    
+    #Título del juego
     screen.blit(titulo_img, [50,-70])
     
     if boton_play.dibujar(screen):
--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Creación de archivo juego "adventure_time.py". Objetivo: correr el juego.
 5/10/2023
 Creación de la clase Boton que permite la configuracion de los botones del menú.
 Se eligen nuevos botones y se modifica un poco de código.
+Se agrega una imagen de título y se agrega fondo sin fin.
+
+6/10/2023
+Se agrega sonido de menú.
