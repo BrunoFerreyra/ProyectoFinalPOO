2391176105e8599924815be8e147650fc6b557ac
maxi-correa <maxis.c.22@gmail.com>
2023-10-07 16:57:04 -0300

    Optimización de código. Actualización de README.

--- Adventure_Time.py
+++ Adventure_Time.py
@@ -46,80 +46,93 @@ boton_play = Boton(300, 250, play_img, 1)
 boton_sound_on = Boton (520, 350, sound_on_img, 1.4)
 boton_sound_off = Boton(620, 350, sound_off_img, 1.4)
 boton_quit = Boton(300, 450, quit_img, 1)
+boton_menu = Boton(750, 10, menu_img, 1)
+boton_resume = Boton(320, 300, restart_img, 1.5)
+boton_sound_on_2 = Boton(470, 400, sound_on_2_img, 1.5)
+boton_sound_off_2 = Boton(550, 400, sound_off_2_img, 1.5)
+#boton_option_2 = Boton(320, 400, option_2_img, 1.5)
+boton_quit_2 = Boton(320, 500, quit_2_img, 1.5)
 
+#Marcadores de inicio
+tema_menu_encendido = True
+juego_corriendo = False
 juego_pausado = False
 run = True
+
+#loop de ejecución
 while run:
     
-    #Fondo en movimiento
-    movimiento_fondo_relativo = movimiento_fondo % fondo_img.get_rect().width
-    screen.blit(fondo_img, (movimiento_fondo_relativo - fondo_img.get_rect().width,0))
-    if movimiento_fondo_relativo < PANTALLA_ANCHO:
-        screen.blit(fondo_img, (movimiento_fondo_relativo,0))
-    movimiento_fondo -= 1
+    #Entrada al menú principal
+    if juego_corriendo == False and juego_pausado == False:
+        
+        #Fondo en movimiento
+        movimiento_fondo_relativo = movimiento_fondo % fondo_img.get_rect().width
+        screen.blit(fondo_img, (movimiento_fondo_relativo - fondo_img.get_rect().width,0))
+        if movimiento_fondo_relativo < PANTALLA_ANCHO:
+            screen.blit(fondo_img, (movimiento_fondo_relativo, 0))
+        movimiento_fondo -= 1
+        
+        #Título del juego
+        screen.blit(titulo_img, [50, -70])
+        screen.blit(option_img, [300, 350])
+        
+        #Condiciones de los botones del menú
+        if boton_play.dibujar(screen):
+            juego_corriendo = True
+            menu_theme.stop()
+            tema_batalla_encendido = True
+            battle_theme.play(-1)
+        
+        #if boton_option.dibujar(screen):
+        #    print('OPTION')
+        
+        if boton_sound_on.dibujar(screen):
+            if tema_menu_encendido == False:
+                tema_menu_encendido = True
+                menu_theme.play(-1)
+        
+        if boton_sound_off.dibujar(screen):
+            tema_menu_encendido = False
+            menu_theme.stop()
+        
+        if boton_quit.dibujar(screen):
+            run = False
     
-    #Título del juego
-    screen.blit(titulo_img, [50, -70])
-    screen.blit(option_img, [300, 350])
+    #Se comienza a ejecutar el juego
+    if juego_corriendo:
+        
+        screen.fill(BLACK)
+        
+        #CODIGO DE JUEGO
+        
+        if boton_menu.dibujar(screen):
+            juego_corriendo = False
+            juego_pausado = True
     
-    if boton_play.dibujar(screen):
-        boton_menu = Boton(750, 10, menu_img, 1)
-        menu_theme.stop()
-        battle_theme.play(-1)
-        play = True
-        while play:
-            
-            screen.fill(BLACK)
-            
-            if boton_menu.dibujar(screen):
-                juego_pausado = True
-                
-                boton_resume = Boton(320, 300, restart_img, 1.5)
-                boton_sound_on_2 = Boton(470, 400, sound_on_2_img, 1.5)
-                boton_sound_off_2 = Boton(550, 400, sound_off_2_img, 1.5)
-                #boton_option_2 = Boton(320, 400, option_2_img, 1.5)
-                boton_quit_2 = Boton(320, 500, quit_2_img, 1.5)
-                while juego_pausado:
-                    screen.fill(BLUE)
-                    screen.blit(titulo_img, [50,-70])
-                    screen.blit(pygame.transform.scale(option_2_img, (135,55)), [320, 400])
-                    if boton_resume.dibujar(screen):
-                        juego_pausado = False
-                    if boton_sound_on_2.dibujar(screen):
-                        battle_theme.play(-1)
-                    if boton_sound_off_2.dibujar(screen):
-                        battle_theme.stop()
-                    #if boton_option_2.dibujar(screen):
-                    #    print('OPTION 2')
-                    if boton_quit_2.dibujar(screen):
-                        pygame.quit()
-                    
-                    for event in pygame.event.get():
-                        if event.type == pygame.QUIT:
-                            pygame.quit()
-                    
-                    pygame.display.update()
-            
-            #CODIGO DE JUEGO
-            
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    play = False
-            pygame.display.update()
-        pygame.quit()
-    
-    #if boton_option.dibujar(screen):
-    #    print('OPTION')
-    
-    if boton_sound_on.dibujar(screen):
-        menu_theme.play(-1)
-    
-    if boton_sound_off.dibujar(screen):
-        menu_theme.stop()
-    
-    
-    if boton_quit.dibujar(screen):
-        run = False
+    #El juego está en estado de pausa
+    if juego_pausado:
+        screen.fill(BLUE)
+        screen.blit(titulo_img, [50,-70])
+        screen.blit(pygame.transform.scale(option_2_img, (135,55)), [320, 400])
+        
+        if boton_resume.dibujar(screen):
+            juego_pausado = False
+            juego_corriendo = True
+        
+        if boton_sound_on_2.dibujar(screen):
+            if tema_batalla_encendido == False:
+                tema_batalla_encendido = True
+                battle_theme.play(-1)
+        
+        if boton_sound_off_2.dibujar(screen):
+            tema_batalla_encendido = False
+            battle_theme.stop()
+        
+        #if boton_option_2.dibujar(screen):
+        #    print('OPTION 2')
+        
+        if boton_quit_2.dibujar(screen):
+            run = False
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
--- README.md
+++ README.md
@@ -11,3 +11,11 @@ Se agrega una imagen de título y se agrega fondo sin fin.
 
 6/10/2023
 Se agrega sonido de menú.
+Se consigue crear un menú principal con configuración básica de sonido y un menú para juego en ejecución
+también con configuración básica de sonido.
+Tenemos el espacio para insertar luego el código del juego propiamente dicho.
+
+7/10/2023
+Se optimiza el código de menús (anteriormente, de un ciclo while, se entraba a otro ciclo while y luego a otro
+para cumplir con los objetivos, ahora se utiliza una serie de marcadores de estado de juego para saber a que porción de código entrar, para un mismo while).
+Al apretar el botón de musica_on se superponían las pistas. Se solucionó con marcadores de encendido, apagado.
